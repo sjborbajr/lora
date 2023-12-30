65c7ec1c904ab06fa67d0a15e7da4480e0babda2
Steve Borba <sjborbajr@users.noreply.github.com>
2023-12-29 19:54:27 -0800

    Update

--- steve1.py
+++ steve1.py
@@ -1,10 +1,11 @@
 import time
 import busio
 from digitalio import DigitalInOut, Direction, Pull
+import sys                                                                                                           |
+import select                                                                                                        |
 import board
 import adafruit_ssd1306
 import adafruit_rfm9x
-import keyboard
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -31,7 +32,6 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 23
 
-recv_prev_packet = "none"
 recv_packet = "none"
 send_packet = "none"
 
@@ -40,47 +40,50 @@ reset_pin = DigitalInOut(board.D4)
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 
 def UpdateDisplay():
-  global recv_prev_packet
   display.fill(0)
   display.text('RX: ', 0, 0, 1)
-  display.text(recv_prev_packet, 25, 0, 1)
+  display.text(recv_packet, 25, 0, 1)
   display.text('TX: ', 0, 10, 1)
   display.text(send_packet, 25, 10, 1)
   display.show()
 
-def send_key(key,event):
+def send_key(key):
   global send_packet
   send_packet = 'Sent Button ' + key + '!'
   rfm9x.send(bytes(send_packet, "ascii"))
   UpdateDisplay()
   print("TX: " + send_packet)
 
-keyboard.on_press_key('a', lambda e: send_key("A", e))
-keyboard.on_press_key('b', lambda e: send_key("B", e))
-keyboard.on_press_key('c', lambda e: send_key("C", e))
 UpdateDisplay()
 while True:
   # check for packet rx
   packet = None
   packet = rfm9x.receive()
-  if not packet is None:
-    # Display the packet text and rssi
+  if packet is not None:
     recv_packet = str(packet, "ascii")
     if recv_packet[:5] == "ACK: ":
       print(recv_packet)
     else:
-      print("RX: "+recv_packet)
-      recv_prev_packet = recv_packet
+      print("RX: '"+recv_packet+"' snr: "+str(rfm9x.last_snr)+" rssi: "+str(rfm9x.last_rssi))
       UpdateDisplay()
-      time.sleep(0.75)
+      time.sleep(0.6)
       temp = "ACK: snr: "+str(rfm9x.last_snr)+" rssi: "+str(rfm9x.last_rssi)
       rfm9x.send(bytes(temp, "ascii"))
 
   if not btnA.value:
-    send_key("A","")
+    send_key("A")
   elif not btnB.value:
-    send_key("B","")
+    send_key("B")
   elif not btnC.value:
-    send_key("C","")
+    send_key("C")
+
+  while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+    char = sys.stdin.read(1)
+    if char == 'a':
+      send_key("A")
+    elif char == 'b':
+      send_key("B")
+    elif char == 'c':
+      send_key("C")
 
   time.sleep(0.1)
