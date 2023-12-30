d6b388f7d6797038fdbe9e1f401dd645bf59f6f7
Steve Borba <sjborbajr@users.noreply.github.com>
2023-12-29 18:33:55 -0800

    sync

--- steve1.py
+++ steve1.py
@@ -55,12 +55,11 @@ while True:
   if not packet is None:
     # Display the packet text and rssi
     recv_packet = str(packet, "ascii")
-    print('"'+recv_packet[:5]+'"')
     if recv_packet[:5] == "ACK: ":
       print(recv_packet)
     else:
-      time.sleep(0.1)
-      temp = "ACK: snr: "+str(rfm9x.last_snr)+" rssi: "+str(rfm9x.last_rssi
+      time.sleep(0.5)
+      temp = "ACK: snr: "+str(rfm9x.last_snr)+" rssi: "+str(rfm9x.last_rssi)
       rfm9x.send(bytes(temp, "ascii"))
       print("RX: "+recv_packet)
       recv_prev_packet = recv_packet
