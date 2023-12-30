c2c5482f666a122907bd0ca365dfc4703bb9e9ed
Steve Borba <sjborbajr@users.noreply.github.com>
2023-12-29 17:40:19 -0800

    share

--- steve1.py
+++ steve1.py
@@ -1,13 +1,10 @@
-# Import Python System Libraries
 import time
-# Import Blinka Libraries
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 import board
-# Import the SSD1306 module.
 import adafruit_ssd1306
-# Import RFM9x
 import adafruit_rfm9x
+import keyboard
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -32,9 +29,7 @@ CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-#classadafruit_rfm9x.RFM9x(spi: SPI, cs: DigitalInOut, reset: DigitalInOut, frequency: int, *, preamble_length: int = 8, high_power: bool = True (Default is True), baudrate: int = 5000000, agc: bool = False, crc: bool = True)
 rfm9x.tx_power = 23
-#The transmit power in dBm. Can be set to a value from 5 to 23 for high power devices (RFM95/96/97/98, high_power=True) or -1 to 14 for low power devices. Only integer power levels are actually set (i.e. 12.5 will result in a value of 12 dBm). The actual maximum setting for high_power=True is 20dBm but for values > 20 the PA_BOOST will be enabled resulting in an additional gain of 3dBm. The actual setting is reduced by 3dBm. The reported value will reflect the reduced setting.
 recv_prev_packet = "none"
 sent_prev_packet = "none"
 recv_packet = "none"
@@ -43,12 +38,14 @@ send_packet = "none"
 # 128x32 OLED Display
 reset_pin = DigitalInOut(board.D4)
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-display.fill(0)
-display.text('RX: ', 0, 0, 1)
-display.text(recv_prev_packet, 25, 0, 1)
-display.text('TX: ', 0, 10, 1)
-display.text(sent_prev_packet, 25, 10, 1)
-display.show()
+
+def UpdateDisplay():
+  display.fill(0)
+  display.text('RX: ', 0, 0, 1)
+  display.text(recv_prev_packet, 25, 0, 1)
+  display.text('TX: ', 0, 10, 1)
+  display.text(sent_prev_packet, 25, 10, 1)
+  display.show()
 
 while True:
   # check for packet rx
@@ -57,36 +54,27 @@ while True:
   if not packet is None:
     # Display the packet text and rssi
     recv_packet = str(packet, "ascii")
-    print("RX: "+recv_packet)
-    rfm9x.send(bytes("snr: "+rfm9x.last_snr+" rssi: "+rfm9x.last_rssi, "ascii"))
-  
-  if not btnA.value:
+    if recv_packet[:5] is "ACK: ":
+      print(recv_packet)
+    else:
+      rfm9x.send(bytes("ACK: snr: "+rfm9x.last_snr+" rssi: "+rfm9x.last_rssi, "ascii"))
+      print("RX: "+recv_packet)
+      recv_prev_packet = recv_packet
+      UpdateDisplay()
+
+  if not btnA.value or keyboard.is_pressed('a'):
     send_packet = 'Sent Button A!'
     rfm9x.send(bytes(send_packet, "ascii"))
-  elif not btnB.value:
+  elif not btnB.value or keyboard.is_pressed('b'):
     send_packet = 'Sent Button B!'
     rfm9x.send(bytes(send_packet, "ascii"))
-  elif not btnC.value:
+  elif not btnC.value or keyboard.is_pressed('c'):
     send_packet = 'Sent Button C!'
     rfm9x.send(bytes(send_packet, "ascii"))
   
-  if not recv_packet is recv_prev_packet:
-    recv_prev_packet = recv_packet
-    display.fill(0)
-    display.text('RX: ', 0, 0, 1)
-    display.text(recv_prev_packet, 25, 0, 1)
-    display.text('TX: ', 0, 10, 1)
-    display.text(sent_prev_packet, 25, 10, 1)
-    display.show()
-
   if not send_packet is sent_prev_packet:
     sent_prev_packet = send_packet
-    display.fill(0)
-    display.text('RX: ', 0, 0, 1)
-    display.text(recv_prev_packet, 25, 0, 1)
-    display.text('TX: ', 0, 10, 1)
-    display.text(sent_prev_packet, 25, 10, 1)
-    display.show()
+    UpdateDisplay()
     print("TX: "+send_packet)
 
   time.sleep(0.1)
